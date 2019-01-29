4425e0a49ed4f43ca35eeb1d2e7a4308dcd5e67e
Ayşenur Kara <aysenurk.95@outlook.com>
2019-01-29 14:42:32 +0300

    market version 2

---
+++ Admin.Models/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Admin.Models.Abstracts;
+
+namespace Admin.Models.Entities
+{
+    [Table("Categories")]
+    public class Category : BaseEntity<int>
+    {
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Kategori adı 3 ile 100 karakter olabilir")]
+        [DisplayName("Kategori Adı")]
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Range(0, 1)]
+        public decimal TaxRate { get; set; } = 0;
+
+        public int SupCategoryId { get; set; }
+
+
+        [ForeignKey("SupCategoryId")]
+        public virtual Category SupCategory { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+        public virtual ICollection<Product> Products { get; set; }=new HashSet<Product>();
+    }
+}
---
+++ Admin.Models/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Admin.Models.Abstracts;
+using Admin.Models.Enums;
+
+namespace Admin.Models.Entities
+{
+    public class Order : BaseEntity<long>
+    {
+        [DisplayName("Sipariş Tipi")]
+        public OrderTypes OrderType { get; set; }
+
+
+        public virtual ICollection<Invoice> Invoices { get; set; }=new HashSet<Invoice>();
+    }
+}
