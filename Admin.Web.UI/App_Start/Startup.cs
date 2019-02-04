074358bc79b4629760cbb2409d628106b60a77ff
Ayşenur Kara <aysenurk.95@outlook.com>
2019-02-04 14:52:56 +0300

    login logout

--- Admin.Web.UI/App_Start/Startup.cs
+++ Admin.Web.UI/App_Start/Startup.cs
@@ -5,21 +5,20 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
 
-[assembly: OwinStartup(typeof(Admin.Web.UI.App_Start.Startup1))]
+[assembly: OwinStartup(typeof(Admin.Web.UI.App_Start.Startup))]
 
 namespace Admin.Web.UI.App_Start
 {
-    public class Startup1
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Login")
+                LoginPath = new PathString("/Account")
             });
         }
-
     }
 }
