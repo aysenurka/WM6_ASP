24ca6ff3f88c3e2f9e30874083688ef3209efdad
Ayşenur Kara <aysenurk.95@outlook.com>
2019-02-06 10:38:15 +0300

    authorize ve logout işlemleri

--- Admin.BLL/Identity/MembershipTools.cs
+++ Admin.BLL/Identity/MembershipTools.cs
@@ -1,4 +1,5 @@
-﻿using Admin.DAL;
+﻿using System.Web;
+using Admin.DAL;
 using Admin.Models.IdentityModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -13,6 +14,27 @@ namespace Admin.BLL.Identity
        public static UserManager<User> NewUserManager() => new UserManager<User>(NewUserStore());
        public static RoleStore<Role> NewRoleStore() => new RoleStore<Role>(_db ?? new MyContext());
        public static RoleManager<Role> NewRoleManager() => new RoleManager<Role>(NewRoleStore());
+
+       public static string GetNameSurname(string userId)
+       {
+           User user;
+           if (string.IsNullOrEmpty(userId))
+           {
+               var id = HttpContext.Current.User.Identity.GetUserId();
+               if (string.IsNullOrEmpty(id))
+                   return "";
+
+               user = NewUserManager().FindById(id);
+           }
+           else
+           {
+               user = NewUserManager().FindById(userId);
+               if (user == null)
+                   return null;
+           }
+
+           return $"{user.Name} {user.Surname}";
+       }
 
     }
 }
