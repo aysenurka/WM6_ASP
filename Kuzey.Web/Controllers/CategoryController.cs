1a7128eefd2ec83297052fba2091f36414726a34
Ayşenur Kara <aysenurk.95@outlook.com>
2019-02-12 14:44:15 +0300

    route config

--- Kuzey.Web/Controllers/CategoryController.cs
+++ Kuzey.Web/Controllers/CategoryController.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using AutoMapper;
 using Kuzey.BLL.Repository;
 using Kuzey.Models.Entities;
 using Kuzey.Models.ViewModels;
+using Kuzey.Web.App_Code;
 
 namespace Kuzey.Web.Controllers
 {
+    [ExceptionHandlerFilter]
+    [RoutePrefix("Kategori")]
     public class CategoryController : Controller
     {
         // GET: Category
+        [Route]
         public ActionResult Index()
         {
             var data = new CategoryRepo().GetAll().Select(x => Mapper.Map<CategoryViewModel>(x)).ToList();
             return View(data);
         }
+
         [HttpPost]
         public ActionResult Add(CategoryViewModel model)
         {
             new CategoryRepo().Insert(Mapper.Map<CategoryViewModel, Category>(model));
             return View();
+        }
+
+        [HttpGet]
+        [Route("~/Detay/{kategoriadi}/{id?}")]
+        public ActionResult Detail(int id = 0)
+        {
+            var model = new CategoryRepo().GetById(id);
+            var data = Mapper.Map<CategoryViewModel>(model);
+            return View(data);
         }
     }
 }
