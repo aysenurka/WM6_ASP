99d6ed8b3e8bafeb7bfab61ac84b9002d8fb6115
Ayşenur Kara <aysenurk.95@outlook.com>
2019-02-27 14:54:35 +0300

    Merhaba! Ben AngularJS öğreniyorum.

---
+++ WebApiAngularJs/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", []);
+
+app.controller("ProdyctCtrl", function($scope) {
+	$scope.urunler = [];
+	$scope.ekle = function() {
+		$scope.urunler.push({
+			id: guid(),
+			urunAdi: $scope.yeni.urunAdi,
+			fiyat: $scope.yeni.fiyat,
+			eklenmeZamani: new Date()
+		});
+	};
+
+	function guid() {
+
+	}
+});
