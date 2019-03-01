18667237e09a44776aa8e1d919dc8b15e36cca92
Ayşenur Kara <aysenurk.95@outlook.com>
2019-03-01 09:30:44 +0300

    postman ile category add

--- WebApiAngularJs/Controllers/CategoryController.cs
+++ WebApiAngularJs/Controllers/CategoryController.cs
@@ -8,11 +8,11 @@ namespace WebApiAngularJs.Controllers
 {
     public class CategoryController : ApiController
     {
+        NorthwindEntities db = new NorthwindEntities();
         public IHttpActionResult GetAll()
         {
             try
             {
-                NorthwindEntities db = new NorthwindEntities();
                 return Ok(new
                 {
                     success = true,
@@ -29,8 +29,61 @@ namespace WebApiAngularJs.Controllers
                 return BadRequest($"Bir hata oluştu {ex.Message}");
             }
         }
-    }
 
+        public IHttpActionResult Get(int id = 0)
+        {
+            try
+            {
+                var cat = db.Categories.Find(id);
+                if (cat == null)
+                {
+                    return NotFound();
+                }
+
+                var data=new CategoryViewModel()
+                {
+                    CategoryID = cat.CategoryID,
+                    CategoryName = cat.CategoryName,
+                    Description = cat.Description
+                };
+
+                return Ok(new
+                {
+                    success = true,
+                    data = data
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] CategoryViewModel model)
+        {
+            try
+            {
+                db.Categories.Add(new Category()
+                {
+                    CategoryName = model.CategoryName,
+                    Description = model.Description
+                });
+                db.SaveChanges();
+
+                return Ok(new
+                {
+                    success = true,
+                    message="Kategori ekleme işlemi başarılı"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+    
     public class CategoryViewModel
     {
         public int CategoryID { get; set; }
