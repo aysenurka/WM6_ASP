c4eb455efe6363762a1150bb2d8304ac1da6be32
Ayşenur Kara <aysenurk.95@outlook.com>
2019-03-06 11:33:00 +0300

    katmanlar ve referanslar tamamlandı. evladiyelik repo oluşturuldu

---
+++ Rabbit.Models/Entities/MailLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rabbit.Models.Entities
+{
+    [Table("MailLogs")]
+    public class MailLog
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Message { get; set; }
+
+        public string Subject { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+    }
+}
